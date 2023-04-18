a44d9c3ad99b1fe10a55f71d6d24cd9d45e56e09
Stanislav <artemiev.stanislav@gmail.com>
2023-04-18 21:54:54 +0700

    Доработка моделей (рабочий запуск)

--- src/main/java/ru/skypro/diplom/repository/AdsRepository.java
+++ src/main/java/ru/skypro/diplom/repository/AdsRepository.java
@@ -1,8 +1,10 @@
 package ru.skypro.diplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.skypro.diplom.model.Ads;
 
+@Repository
 public interface AdsRepository extends JpaRepository<Ads,Integer> {
 
 }
--- src/main/java/ru/skypro/diplom/repository/CommentRepository.java
+++ src/main/java/ru/skypro/diplom/repository/CommentRepository.java
@@ -1,8 +1,10 @@
 package ru.skypro.diplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.skypro.diplom.model.Comment;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 
 }
